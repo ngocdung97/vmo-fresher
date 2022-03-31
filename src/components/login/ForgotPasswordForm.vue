c0e48808ee29451e1c81a1e0bab12bee29402397
thanhtung0310 <thanhtung031099@gmail.com>
2022-03-31 10:59:21 +0700

    draw login register forgot-password form

---
+++ src/components/login/ForgotPasswordForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <form>
+      <h2 class="card-header">
+        FORGOT PASSWORD
+      </h2>
+
+      <div class="form-group">
+        <label>Username</label> <br>
+        <input
+          type="text"
+          placeholder="Type your username..."
+        >
+      </div>
+
+      <button
+        type="submit"
+        @click="onLoginForm"
+      >
+        Reset Password
+      </button>
+    </form>
+  </div>  
+</template>
+
+<script>
+export default {
+  methods: {
+    onLoginForm () {
+      this.$router.push('/log-in')
+    }
+  },
+}
+</script>
+
+<style lang="css">  
+</style>
--- src/components/login/RegisterForm.vue
+++ src/components/login/RegisterForm.vue
@@ -1,21 +1,69 @@
 <template>
-  <div>
-    register
-    <button @click="onRegisterForm">
-      Sang form Login
-    </button>
+  <div class="card">
+    <form>
+      <h2 class="card-header">
+        REGISTER
+      </h2>
+
+      <div class="form-group">
+        <label>First Name</label> <br>
+        <input type="text">
+      </div>
+
+      <div class="form-group">
+        <label>Last Name</label> <br>
+        <input type="text">
+      </div>
+
+      <div class="form-group">
+        <label>Department</label> <br>
+        <input type="text">
+      </div>
+
+      <div class="form-group">
+        <label>Username</label> <br>
+        <input type="text">
+      </div>
+
+      <div class="form-group">
+        <label>Password</label> <br>
+        <input type="text">
+      </div>
+
+      <div class="form-group">
+        <label>Confirm Password</label> <br>
+        <input type="text">
+      </div>
+
+      <div class="form-group">
+        <label>E-mail</label> <br>
+        <input type="text">
+      </div>
+
+      <div class="form-group">
+        <label>Contact Number</label> <br>
+        <input type="text">
+      </div>
+
+      <button 
+        type="submit" 
+        @click="onLoginForm"
+      >
+        Register
+      </button>
+    </form>
   </div>
 </template>
 <script>
 export default {
   methods: {
-    onRegisterForm () {
-      this.$router.push('/')
+    onLoginForm () {
+      this.$router.push('/log-in')
       //   this.$emit('onChangeForm', 'register')
-      debugger
+      // debugger
     }
   },
 }
 </script>
-<style lang="scss" scoped>
+<style lang="css">
 </style>
